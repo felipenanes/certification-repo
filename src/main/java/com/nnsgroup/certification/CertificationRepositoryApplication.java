3f8b370193a9e0e405dad8d4e292d8346e5a73b9
fnanestechnl <fnanes@techseals.nl>
2025-06-20 11:51:03 +0200

    first commit

---
+++ src/main/java/com/nnsgroup/certification/CertificationRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.nnsgroup.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnsgroup/certification/CertificationRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnsgroup.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
